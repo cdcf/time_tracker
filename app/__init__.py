3a87e2865d079c4b753aec90a8bfe1eda5820aa0
cdcf <cedric@dacostafaro.com>
2014-08-07 18:22:47 +0200

    User model (v0.5)
    Addition of db user model + def of SQLAlchemy use

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
 from config import config
 
 
 bootstrap = Bootstrap()
+
+db = SQLAlchemy
 
 
 def create_app(config_name):
@@ -11,6 +14,7 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
 
     bootstrap.init_app(app)
+    db.init_app(app)
 
     from .projects import projects as projects_blueprint
     app.register_blueprint(projects_blueprint)
--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 __author__ = 'Cedric Da Costa Faro'
 
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
@@ -10,15 +11,20 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 't0p s3cr3t'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
 class TestingConfig(Config):
     TESTING = True
     SECRET_KEY = 'secret'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
 
 class ProductionConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
 config = {
