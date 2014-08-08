7fbc902b2b76f84921f52f29eb49631be1ce24be
cdcf <cedric@dacostafaro.com>
2014-08-08 08:57:14 +0200

    Register new admin users from command line (v0.8)
    A way to register admin user from command line only.

--- manage.py
+++ manage.py
@@ -1,12 +1,29 @@
 __author__ = 'Cedric Da Costa Faro'
 
-#!/usr/bin/env python
 import os
 from app import create_app
 from flask.ext.script import Manager
+from app import db
+from app.models import User
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+
+
+@manager.command
+def adduser(email, username, admin=True):
+    """Register an admin user only from command line"""
+    from getpass import getpass
+    password = getpass()
+    password2 = getpass(prompt='Confirm: ')
+    if password != password2:
+        import sys
+        sys.exit('Error: passwords do not match.')
+    db.create_all()
+    user = User(email=email, username=username, password=password, is_admin=admin)
+    db.session.add(user)
+    db.session.commit()
+    print('User {0} was registered successfully.'.format(username))
 
 
 if __name__ == '__main__':
