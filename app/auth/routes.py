4d0888abcfa8f46949c1c5433924a8912b2d8409
cdcf <cedric@dacostafaro.com>
2014-08-08 09:20:48 +0200

    Login form for registered users (v0.9)
    Quick login form to get connected - users must have been created first
    at this stage.

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,8 +1,14 @@
 __author__ = 'Cedric Da Costa Faro'
 
-from flask import render_template
+from flask import render_template, current_app, request, redirect, url_for
 from . import auth
+from .forms import LoginForm
 
-@auth.route('/login/')
+@auth.route('/login/', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html/')
+    if not current_app.config['DEBUG'] and not current_app.config['TESTING'] and not request.is_secure:
+        return redirect(url_for('auth.login', _external=True, _scheme='https'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('auth/login.html/', form=form)
