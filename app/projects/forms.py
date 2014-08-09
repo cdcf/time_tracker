ede1f5dae0cdca8cc2594e3fbc7dd3d920e77e6d
cdcf <cedric@dacostafaro.com>
2014-08-09 10:26:31 +0200

    correction on user profile
    class ProfileForm wrongly named - updated

--- app/projects/forms.py
+++ app/projects/forms.py
@@ -4,7 +4,7 @@ from flask.ext.wtf import Form
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Optional, Length
 
-class Profile(Form):
+class ProfileForm(Form):
     name = StringField('Name', validators=[Optional(), Length(1, 64)])
     location = StringField('Location', validators=[Optional(), Length(1, 64)])
     bio = TextAreaField('Bio')
