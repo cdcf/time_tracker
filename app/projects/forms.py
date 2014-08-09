a395633b4a76678d780d1ea439b6d1116bc0da4c
cdcf <cedric@dacostafaro.com>
2014-08-09 15:41:36 +0200

    Edit projects (v0.17)
    It is now possible to edit projects

--- app/projects/forms.py
+++ app/projects/forms.py
@@ -15,8 +15,19 @@ class ProfileForm(Form):
 
 
 # We define here the structure of a project, typically, the project name, a brief description and a date when it starts
+# and we allow that it is updated if required
 class ProjectForm(Form):
     title = StringField('Title', validators=[Required(), Length(1, 128)])
     description = TextAreaField('Desciption')
-    date = DateField('Date', format='%d/%m/%Y')
+    date = DateField('Date', format='%d/%m/%Y', placeholder='dd/mm/yyyy')
     submit = SubmitField('Submit')
+
+    def from_model(self, project):
+        self.title.data = project.title
+        self.description.data = project.description
+        self.date.data = project.date
+
+    def to_model(self, project):
+        project.title = self.title.data
+        project.description = self.description.data
+        project.date = self.date.data
--- app/projects/routes.py
+++ app/projects/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, abort
 from flask.ext.login import login_required, current_user
 from .. import db
 from ..models import User, Project
@@ -42,16 +42,14 @@ def profile():
     return render_template('projects/profile.html/', form=form)
 
 
-# We allow here a user to create a new project once he has logged in
+# We allow here a user to create a new project once he has logged in and to modify his own ones only
 @projects.route('/new/', methods=['GET', 'POST'])
 @login_required
 def new_project():
     form = ProjectForm()
     if form.validate_on_submit():
-        project = Project(title=form.title.data,
-                          description=form.description.data,
-                          date=form.date.data,
-                          author=current_user)
+        project = Project(author=current_user)
+        form.to_model(project)
         db.session.add(project)
         db.session.commit()
         flash('The project was added successfully.')
@@ -59,7 +57,29 @@ def new_project():
     return render_template('projects/edit_project.html/', form=form)
 
 
+# We list here all projects
 @projects.route('/project/<int:id>/')
 def project(id):
     project = Project.query.get_or_404(id)
-    return render_template('projects/project.html/', project=project)
+    headers = {}
+    if current_user.is_authenticated():
+        headers['X-XSS-Protection'] = '0'
+    return render_template('projects/project.html/', project=project), 200, headers
+
+
+# We enable project owner to edit them if required
+@projects.route('/edit/<int:id>/', methods=['GET', 'POST'])
+@login_required
+def edit_project(id):
+    project = Project.query.get_or_404(id)
+    if not current_user.is_admin and project.author != current_user:
+        abort(403)
+    form = ProjectForm()
+    if form.validate_on_submit():
+        form.to_model(project)
+        db.session.add(project)
+        db.session.commit()
+        flash('The project was updated successfully.')
+        return redirect(url_for('projects.project', id=project.id))
+    form.from_model(project)
+    return render_template('projects/edit_project.html', form=form)
