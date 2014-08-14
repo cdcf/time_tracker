34e48bf512f9034b8469906a5b9bae26fe999789
cdcf <cedric@dacostafaro.com>
2014-08-14 09:19:10 +0200

    Alembic Migration tool implemented (v0.23)
    Alembic migration tool has been implemented

--- manage.py
+++ manage.py
@@ -2,12 +2,20 @@ __author__ = 'Cedric Da Costa Faro'
 
 import os
 from app import create_app
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Shell
+from flask.ext.migrate import Migrate, MigrateCommand
 from app import db
 from app.models import User
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 
 
 # This command line function allows to create an Admin user only. As an admin user has extensive rights, we do not
