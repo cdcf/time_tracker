627428b4cf552ac54fceda67b0ada8d09a8e51ff
cdcf <cedric@dacostafaro.com>
2014-08-19 15:57:23 +0200

    typo on https for Gravatar (v0.24b)

--- app/models.py
+++ app/models.py
@@ -42,7 +42,7 @@ class User(UserMixin, db.Model):
 
     def gravatar(self, size=100, default='identicon', rating='g'):
         if request.is_secure:
-            url = 'htpps://secure.gravatar.com/avatar'
+            url = 'https://secure.gravatar.com/avatar'
         else:
             url = 'http://www.gravatar.com/avatar'
         hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
